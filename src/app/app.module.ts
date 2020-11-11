786e8afa38cbaf5feb08cb3d4ea89b5f2ca887b6
daviti chivadze <datochicho11@yahoo.com>
2020-11-12 01:30:33 +0400

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataSynchronizationComponent } from './Components/Main/data-synchronization/data-synchronization.component';
+import { UserManagmentComponent } from './Components/Main/user-managment/user-managment.component';
+import { DevicesComponent } from './Components/Main/devices/devices.component';
+import { EmployeeComponent } from './Components/Main/employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataSynchronizationComponent,
+    UserManagmentComponent,
+    DevicesComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
