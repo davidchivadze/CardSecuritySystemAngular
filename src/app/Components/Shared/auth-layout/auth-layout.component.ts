afe670cdf493100ea34a1cec6cb742bb77a1636a
daviti chivadze <datochicho11@yahoo.com>
2021-04-07 11:54:27 +0400

    add Auth

--- src/app/Components/Shared/auth-layout/auth-layout.component.ts
+++ src/app/Components/Shared/auth-layout/auth-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Api } from 'src/app/Services/SwaggerClient';
 
 @Component({
   selector: 'app-auth-layout',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(public EmployeeService:Api.EmployeeService,public translate:TranslateService){
+    translate.addLangs(['en','ka','ru']);
+    console.log(this.getLanguage());
+    translate.setDefaultLang(this.getLanguage());
+  }
 
   ngOnInit(): void {
   }
-
+  title = 'ZKTecoAdmin';
+  Language=this.getLanguage();
+   translateWeb(lang:string) {
+     localStorage.setItem('language',lang);
+     this.translate.use(lang);
+     this.Language=lang;
+   }
+   getLanguage():string{
+    console.log(localStorage.getItem('language'))
+     return localStorage.getItem('language')||"en";
+   }
 }
